ce674407788694748dea1b8c13203a158834a781
Nathan ter Bogt <nterbogt@gmail.com>
2025-07-28 14:21:30 +1200

    Reverting the purposeful breakage

--- cmd/apiserver-mock/main.go
+++ cmd/apiserver-mock/main.go
@@ -53,8 +53,6 @@ func main() {
 		RunE: func(_ *cobra.Command, _ []string) error {
 			server := grpc.NewServer()
 
-			foo, _ := myfunc()
-
 			log.Println("Registering service: Compass")
 			pb.RegisterCompassServer(server, &compass.Server{})
 
