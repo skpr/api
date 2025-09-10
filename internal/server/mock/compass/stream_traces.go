14caf424a40541c51d70a5ad223d0f77bc06a0e3
Nick Schuch <nick@previousnext.com.au>
2025-09-10 22:42:16 +1000

    Update Compass mock

--- internal/server/mock/compass/stream_traces.go
+++ internal/server/mock/compass/stream_traces.go
@@ -1,7 +1,7 @@
 package compass
 
 import (
-	"context"
+	"fmt"
 	"time"
 
 	"github.com/brianvoe/gofakeit/v7"
@@ -10,107 +10,117 @@ import (
 )
 
 // StreamTraces streams Compass traces from a specific environment.
-func (s *Server) StreamTraces(ctx context.Context, req *pb.StreamTracesRequest) (*pb.StreamTracesResponse, error) {
+func (s *Server) StreamTraces(_ *pb.StreamTracesRequest, server pb.Compass_StreamTracesServer) error {
 	for {
 		// Simulate some processing delay.
 		time.Sleep(time.Second)
 
-		resp := &pb.StreamTracesResponse{
-			Traces: []*pb.CompassTrace{
-				{
-					Metadata: &pb.CompassTraceMetadata{
-						RequestId: gofakeit.UUID(),
-						Uri:       "/sites/default/files/styles/scale_crop_7_3_wide/public/veggie-pasta-bake-hero-umami.jpg.webp?itok=CYsHBUlX",
-						StartTime: 11479712402527,
-						EndTime:   11480550685871,
+		for {
+			resp := &pb.StreamTracesResponse{
+				Traces: []*pb.CompassTrace{
+					{
+						Metadata: &pb.CompassTraceMetadata{
+							RequestId: gofakeit.UUID(),
+							Uri:       "/sites/default/files/styles/scale_crop_7_3_wide/public/veggie-pasta-bake-hero-umami.jpg.webp?itok=CYsHBUlX",
+							StartTime: 11479712402527,
+							EndTime:   11480550685871,
+						},
+						FunctionCalls: []*pb.CompassTraceFunctionCall{
+							{
+								Name:        "PDOStatement::execute",
+								StartTime:   11479719656578,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\Core\\Database\\StatementPrefetchIterator::execute",
+								StartTime:   11479719664878,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\sqlite\\Driver\\Database\\sqlite\\Statement::execute",
+								StartTime:   11479719666498,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\Core\\Database\\Query\\Upsert::execute",
+								StartTime:   11479719668488,
+								ElapsedTime: 5999966,
+							},
+						},
 					},
-					FunctionCalls: []*pb.CompassTraceFunctionCall{
-						{
-							Name:        "PDOStatement::execute",
-							StartTime:   11479719656578,
-							ElapsedTime: 5999966,
+					{
+						Metadata: &pb.CompassTraceMetadata{
+							RequestId: gofakeit.UUID(),
+							Uri:       "/sites/default/files/styles/scale_crop_7_3_wide/public/veggie-pasta-bake-hero-umami.jpg.webp?itok=CYsHBUlX",
+							StartTime: 11479712402527,
+							EndTime:   11480550685871,
 						},
-						{
-							Name:        "Drupal\\Core\\Database\\StatementPrefetchIterator::execute",
-							StartTime:   11479719664878,
-							ElapsedTime: 5999966,
+						FunctionCalls: []*pb.CompassTraceFunctionCall{
+							{
+								Name:        "PDOStatement::execute",
+								StartTime:   11479719656578,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\Core\\Database\\StatementPrefetchIterator::execute",
+								StartTime:   11479719664878,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\sqlite\\Driver\\Database\\sqlite\\Statement::execute",
+								StartTime:   11479719666498,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\Core\\Database\\Query\\Upsert::execute",
+								StartTime:   11479719668488,
+								ElapsedTime: 5999966,
+							},
 						},
-						{
-							Name:        "Drupal\\sqlite\\Driver\\Database\\sqlite\\Statement::execute",
-							StartTime:   11479719666498,
-							ElapsedTime: 5999966,
+					},
+					{
+						Metadata: &pb.CompassTraceMetadata{
+							RequestId: gofakeit.UUID(),
+							Uri:       "/sites/default/files/styles/scale_crop_7_3_wide/public/veggie-pasta-bake-hero-umami.jpg.webp?itok=CYsHBUlX",
+							StartTime: 11479712402527,
+							EndTime:   11480550685871,
 						},
-						{
-							Name:        "Drupal\\Core\\Database\\Query\\Upsert::execute",
-							StartTime:   11479719668488,
-							ElapsedTime: 5999966,
+						FunctionCalls: []*pb.CompassTraceFunctionCall{
+							{
+								Name:        "PDOStatement::execute",
+								StartTime:   11479719656578,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\Core\\Database\\StatementPrefetchIterator::execute",
+								StartTime:   11479719664878,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\sqlite\\Driver\\Database\\sqlite\\Statement::execute",
+								StartTime:   11479719666498,
+								ElapsedTime: 5999966,
+							},
+							{
+								Name:        "Drupal\\Core\\Database\\Query\\Upsert::execute",
+								StartTime:   11479719668488,
+								ElapsedTime: 5999966,
+							},
 						},
 					},
 				},
-				{
-					Metadata: &pb.CompassTraceMetadata{
-						RequestId: gofakeit.UUID(),
-						Uri:       "/sites/default/files/styles/scale_crop_7_3_wide/public/veggie-pasta-bake-hero-umami.jpg.webp?itok=CYsHBUlX",
-						StartTime: 11479712402527,
-						EndTime:   11480550685871,
-					},
-					FunctionCalls: []*pb.CompassTraceFunctionCall{
-						{
-							Name:        "PDOStatement::execute",
-							StartTime:   11479719656578,
-							ElapsedTime: 5999966,
-						},
-						{
-							Name:        "Drupal\\Core\\Database\\StatementPrefetchIterator::execute",
-							StartTime:   11479719664878,
-							ElapsedTime: 5999966,
-						},
-						{
-							Name:        "Drupal\\sqlite\\Driver\\Database\\sqlite\\Statement::execute",
-							StartTime:   11479719666498,
-							ElapsedTime: 5999966,
-						},
-						{
-							Name:        "Drupal\\Core\\Database\\Query\\Upsert::execute",
-							StartTime:   11479719668488,
-							ElapsedTime: 5999966,
-						},
-					},
-				},
-				{
-					Metadata: &pb.CompassTraceMetadata{
-						RequestId: gofakeit.UUID(),
-						Uri:       "/sites/default/files/styles/scale_crop_7_3_wide/public/veggie-pasta-bake-hero-umami.jpg.webp?itok=CYsHBUlX",
-						StartTime: 11479712402527,
-						EndTime:   11480550685871,
-					},
-					FunctionCalls: []*pb.CompassTraceFunctionCall{
-						{
-							Name:        "PDOStatement::execute",
-							StartTime:   11479719656578,
-							ElapsedTime: 5999966,
-						},
-						{
-							Name:        "Drupal\\Core\\Database\\StatementPrefetchIterator::execute",
-							StartTime:   11479719664878,
-							ElapsedTime: 5999966,
-						},
-						{
-							Name:        "Drupal\\sqlite\\Driver\\Database\\sqlite\\Statement::execute",
-							StartTime:   11479719666498,
-							ElapsedTime: 5999966,
-						},
-						{
-							Name:        "Drupal\\Core\\Database\\Query\\Upsert::execute",
-							StartTime:   11479719668488,
-							ElapsedTime: 5999966,
-						},
-					},
-				},
-			},
+			}
+
+			err := server.Send(resp)
+			if err != nil {
+				fmt.Println("Stopping log stream for:", err.Error())
+				break
+			}
+
+			time.Sleep(500 * time.Millisecond)
 		}
 
 		// Simulate sending the response back to the client.
-		return resp, nil
+		return nil
 	}
 }
