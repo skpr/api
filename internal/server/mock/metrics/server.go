68ea855c444970993e1198d9d8f5e4a5f849fdbd
Nathan ter Bogt <nterbogt@gmail.com>
2026-04-14 15:03:34 +1200

    feat: Application metrics and support undefined metrics (#53)

--- internal/server/mock/metrics/server.go
+++ internal/server/mock/metrics/server.go
@@ -4,8 +4,7 @@ import (
 	"context"
 	"fmt"
 	"hash/fnv"
-	"maps"
-	"slices"
+	"strings"
 	"time"
 
 	"github.com/skpr/api/pb"
@@ -17,6 +16,46 @@ import (
 // Server implements the GRPC "events" definition.
 type Server struct {
 	pb.UnimplementedMetricsServer
+}
+
+type MockMetric struct {
+	Min    float64
+	Max    float64
+	Source pb.MetricSource
+}
+
+// metricData is built once at package initialization
+var metricData = map[pb.MetricType]map[string]MockMetric{
+	pb.MetricType_CLUSTER: {
+		"requests":            {250_000, 4_000_000, pb.MetricSource_SYSTEM},
+		"httpcode_target_200": {500, 1_000, pb.MetricSource_SYSTEM},
+		"httpcode_target_300": {25, 50, pb.MetricSource_SYSTEM},
+		"httpcode_target_400": {10, 25, pb.MetricSource_SYSTEM},
+		"httpcode_target_500": {0, 10, pb.MetricSource_SYSTEM},
+	},
+	pb.MetricType_ENVIRONMENT: {
+		"requests":              {25_000, 200_000, pb.MetricSource_SYSTEM},
+		"cpu":                   {25, 100, pb.MetricSource_SYSTEM},
+		"memory":                {512_000, 4_294_967_296, pb.MetricSource_SYSTEM}, // 512KB to 4GB
+		"replicas":              {2, 8, pb.MetricSource_SYSTEM},
+		"php_active":            {4, 48, pb.MetricSource_SYSTEM},
+		"php_idle":              {2, 12, pb.MetricSource_SYSTEM},
+		"php_queued":            {0, 8, pb.MetricSource_SYSTEM},
+		"cache_hit_rate":        {60, 99, pb.MetricSource_SYSTEM},
+		"invalidation_paths":    {10, 50, pb.MetricSource_SYSTEM},
+		"invalidation_requests": {5, 20, pb.MetricSource_SYSTEM},
+		"origin_errors":         {0, 10, pb.MetricSource_SYSTEM},
+		"httpcode_target_200":   {200, 500, pb.MetricSource_SYSTEM},
+		"httpcode_target_300":   {25, 50, pb.MetricSource_SYSTEM},
+		"httpcode_target_400":   {10, 25, pb.MetricSource_SYSTEM},
+		"httpcode_target_500":   {0, 10, pb.MetricSource_SYSTEM},
+		"response_times_avg":    {0.1, 0.25, pb.MetricSource_SYSTEM},
+		"response_times_p95":    {2.0, 5.0, pb.MetricSource_SYSTEM},
+		"response_times_p99":    {10.0, 20.0, pb.MetricSource_SYSTEM},
+		"mock_application_1":    {0, 100, pb.MetricSource_APPLICATION},
+		"mock_application_2":    {0.3, 1.4, pb.MetricSource_APPLICATION},
+		"mock_application_3":    {0, 100_000_000, pb.MetricSource_APPLICATION},
+	},
 }
 
 // deterministicRange returns a specific value consistently for a point in a time series.
@@ -31,54 +70,31 @@ func deterministicRange(t time.Time, minVal, maxVal float64, seconds int64, key 
 	return minVal + (hashVal * rangeSize)
 }
 
-func metricMappings(metricType pb.MetricType) map[string][]float64 {
-	data := map[pb.MetricType]map[string][]float64{
-		pb.MetricType_CLUSTER: {
-			"requests":            {250_000, 4_000_000},
-			"httpcode_target_200": {500, 1_000},
-			"httpcode_target_300": {25, 50},
-			"httpcode_target_400": {10, 25},
-			"httpcode_target_500": {0, 10},
-		},
-		pb.MetricType_ENVIRONMENT: {
-			"requests":              {25_000, 200_000},
-			"cpu":                   {25, 100},
-			"memory":                {512_000, 4_294_967_296}, // 512KB to 4GB
-			"replicas":              {2, 8},
-			"php_active":            {4, 48},
-			"php_idle":              {2, 12},
-			"php_queued":            {0, 8},
-			"cache_hit_rate":        {60, 99},
-			"invalidation_paths":    {10, 50},
-			"invalidation_requests": {5, 20},
-			"origin_errors":         {0, 10},
-			"httpcode_target_200":   {200, 500},
-			"httpcode_target_300":   {25, 50},
-			"httpcode_target_400":   {10, 25},
-			"httpcode_target_500":   {0, 10},
-			"response_times_avg":    {0.1, 0.25},
-			"response_times_p95":    {2.0, 5.0},
-			"response_times_p99":    {10.0, 20.0},
-		},
-	}
-	return data[metricType]
+func metricMappings(metricType pb.MetricType) map[string]MockMetric {
+	return metricData[metricType]
 }
 
 // AvailableMetrics lists all available metrics.
 func (s *Server) AvailableMetrics(ctx context.Context, req *pb.AvailableMetricsRequest) (*pb.AvailableMetricsResponse, error) {
 	mappings := metricMappings(req.Type)
-	keys := slices.Collect(maps.Keys(mappings))
 
-	metrics := make([]*pb.MetricDefinition, len(keys))
-	for i, key := range keys {
-		metrics[i] = &pb.MetricDefinition{
-			Name: key,
-			Type: req.Type,
+	availableMetrics := []*pb.MetricDefinition{}
+	for key, metric := range mappings {
+		// Hide application metrics for the prod environment.
+		if req.Environment != nil && *req.Environment == "prod" && metric.Source == pb.MetricSource_APPLICATION {
+			continue
 		}
+
+		availableMetrics = append(availableMetrics, &pb.MetricDefinition{
+			Name:   key,
+			Type:   req.Type,
+			Title:  strings.ReplaceAll(key, "_", " "),
+			Source: metric.Source,
+		})
 	}
 
 	return &pb.AvailableMetricsResponse{
-		Metrics: metrics,
+		Metrics: availableMetrics,
 	}, nil
 }
 
@@ -88,15 +104,15 @@ func (s *Server) AbsoluteRange(ctx context.Context, req *pb.AbsoluteRangeRequest
 	if _, ok := mappings[req.Metric]; !ok {
 		return nil, status.Errorf(codes.NotFound, "metric %s not found", req.Metric)
 	}
-	metricMin, metricMax := mappings[req.Metric][0], mappings[req.Metric][1]
+	metricMin, metricMax := mappings[req.Metric].Min, mappings[req.Metric].Max
 
 	output := []*pb.MetricValue{}
+	seedKey := fmt.Sprintf("%s_%s", req.Type, req.Metric)
 	metricTime := req.StartTime.AsTime()
 	for metricTime.Before(req.EndTime.AsTime()) {
-		cacheKey := fmt.Sprintf("%s_%s", req.Type, req.Metric)
 		metric := pb.MetricValue{
 			Timestamp: timestamppb.New(metricTime),
-			Value:     deterministicRange(metricTime, metricMin, metricMax, 60, cacheKey),
+			Value:     new(deterministicRange(metricTime, metricMin, metricMax, 60, seedKey)),
 		}
 		output = append(output, &metric)
 		metricTime = metricTime.Add(time.Minute)
