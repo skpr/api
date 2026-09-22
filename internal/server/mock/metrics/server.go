93c3cf9c0c65c0b4bf4064df57631dcf627e6bff
Nathan ter Bogt <nterbogt@gmail.com>
2026-09-22 20:43:58 +1200

    chore: Golangci implementation (#77)
    * Initial setup of golangci
    
    * Use the action for installing
    
    * Fixing the int32 issue and surpressing a crypto safe warning.
    
    * Include errcheck issues too
    
    * Renaming workflow
    
    * Even better naming

--- internal/server/mock/metrics/server.go
+++ internal/server/mock/metrics/server.go
@@ -7,10 +7,11 @@ import (
 	"strings"
 	"time"
 
-	"github.com/skpr/api/pb"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"github.com/skpr/api/pb"
 )
 
 // Server implements the GRPC "events" definition.
